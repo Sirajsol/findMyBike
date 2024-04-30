c347e90ac42527d75e40ffa1039962f00c797905
Siraj soliman <healthtartus@gmail.com>
2024-04-30 21:24:35 +0300

    frthhh

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,7 @@ import styles from'./page.module.css'
 const Footer=()=>{
     return<>
     <div className={styles.cont}>
-    <div className={styles.txt}>{`Sitaj Soliman 2024 `}</div>
+    <div className={styles.txt}>{`Siraj Soliman 2024 `}</div>
 
     </div>
     </>
